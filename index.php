791cd9eaee1479ade39daddd10f3cf1766dd653f
Samguzman30 <19samuelguzman@gmail.com>
2022-04-17 13:39:14 -0300

    cambios

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
                             <a class="nav-link px-4 active" aria-current="page" href="index.html">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="sobre-mi.html">Sobre Mi</a>
+                            <a class="nav-link" href="sobre-mi.php">Sobre Mi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4" href="contacto.html">Contacto</a>
+                            <a class="nav-link px-4" href="contacto.php">Contacto</a>
                         </li>
                     </ul>
                     <div class="d-flex">
