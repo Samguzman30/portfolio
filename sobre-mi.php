e6d05d9d692730d7cc9df0da66e4fd4db874bf2d
Samguzman30 <19samuelguzman@gmail.com>
2022-04-19 00:30:07 -0300

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -39,13 +39,80 @@
                         <a class="btn btnrojo">Descargar mi CV <i class="fa-solid fa-file-arrow-down"></i></a>
                     </div>
     </header>
-    <main class="container">
+    <main>
+        <div class="container">
     <div class="row">
-        <div class="col-12">
+        <div class="col-6 col-sm-5 mt-4">
     <h1>Sobre Mi</h1>
+    <p class="pb-sm-5">Apasionado por la tecnología y gestión de proyectos. Soy estudiante de cursos de programación Full Stack y de Base de datos.</p>
+        <a href="contacto.php" class="btn btnrojo">Enviar mensaje</a>
         </div>
-
+        <div class="col-6">
+            <img class="perfil" src="images/IMGSAMU.png" alt="sobre-mi">
+        </div>
     </div>
+    </div>
+    <section class="tecnologias pb-5">
+    <div class="container stack">
+        <div class="row">
+            <div class="col-12 mt-5 mb-5">
+                <h2>Stack tecnológico</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5 me-3">
+        <h3>Javascript</h3>
+        <img src="images/javascript.jpeg" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5 me-3">
+        <h3>PHP</h3>
+        <img src="images/php.jpg" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5 me-3">
+        <h3>HTML5</h3>
+        <img src="images/html.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5 me-3">
+        <h3>React.js</h3>
+        <img src="images/react.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5 me-3">
+        <h3>j.Query</h3>
+        <img src="images/jquery.jpg" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-2 pe-2 pt-5 pb-5">
+        <h3>Boostrap</h3>
+        <img src="images/boostrap.png" class="tecno-image" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5 me-3">
+        <h3>Laravel</h3>
+        <img src="images/laravel.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5 me-3">
+        <h3>MySQL</h3>
+        <img src="images/mariadb.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5 me-3">
+        <h3>CSS</h3>
+        <img src="images/css.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5 me-3">
+        <h3>Git</h3>
+        <img src="images/git.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5 me-3">
+        <h3>Apache</h3>
+        <img src="images/Apache.png" class="tecno-image" alt="">
+            </div>
+            <div class=" tecno col-2 text-center mt-4 pe-2 pt-5 pb-5">
+        <h3>Mercadopago</h3>
+        <img src="images/mercadopago.jpg" class="tecno-image" alt="">
+            </div>
+        </div>
+    </div>
+    </section>
     </main>
 </body>
 </html>
