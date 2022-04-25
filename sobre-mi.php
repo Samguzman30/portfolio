8eea37952e84dfeff2616158625f37c755d4c002
Samguzman30 <19samuelguzman@gmail.com>
2022-04-24 21:03:16 -0300

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,6 @@
+<?php
+$pg = "sobre-mi";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,32 +16,7 @@
 </head>
 
 <body id="sobre-mi">
-    <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-4" aria-current="page" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 active" href="sobre-mi.php">Sobre mi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex">
-                        <a class="btn btnrojo">Descargar mi CV <i class="fa-solid fa-file-arrow-down"></i></a>
-                    </div>
-    </header>
+<?php include_once ("header.php") ?>;
     <main>
         <div class="container">
     <div class="row">
